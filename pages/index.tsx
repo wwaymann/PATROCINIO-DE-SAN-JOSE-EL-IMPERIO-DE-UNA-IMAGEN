097ede5dfe824f1b715372c7e4501de890ce882b
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-11-12 23:47:33 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -513,15 +513,14 @@ END:VCALENDAR`
             <div>
               <h2>registro al lanzamiento</h2>
               <p className="lede">
-                Completa el formulario para confirmar tu asistencia y recibir información del
-                lanzamiento.
+                Completa el formulario para confirmar tu asistencia.
               </p>
 
               <div className="card" style={{ padding: 0, overflow: 'hidden' }}>
                 <iframe
                   src="https://docs.google.com/forms/d/e/1FAIpQLSchxaUg9AjWHibY7OniBQ5eQugSJNRz6Y1fm6sJWUUjzjyYig/viewform?embedded=true"
                   width="100%"
-                  height={800}
+                  height={900}
                   frameBorder={0}
                   marginHeight={0}
                   marginWidth={0}
