644bd414b7f776dade01c8bb082840218041060f
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-11-18 18:49:27 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -505,9 +505,15 @@ END:VCALENDAR`
       </p>
     </aside>
   </div>
+            <div 
+              style="position:relative;padding-top:max(60%,324px);width:100%;height:0;"><iframe 
+              style="position:absolute;border:none;width:100%;height:100%;left:0;top:0;" 
+              src="https://online.fliphtml5.com/ArenaDigital/jfgs/"  seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" >
+              </iframe>
+            </div>
+
 </section>
 
-          <div style="position:relative;padding-top:max(60%,324px);width:100%;height:0;"><iframe style="position:absolute;border:none;width:100%;height:100%;left:0;top:0;" src="https://online.fliphtml5.com/ArenaDigital/jfgs/"  seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" ></iframe></div>
 
        
 
