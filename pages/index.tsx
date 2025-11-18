afe467304a7d4507cd4790884b316798e40160fb
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-11-18 18:26:14 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -462,7 +462,11 @@ END:VCALENDAR`
               la Doctora en Historia del Arte Constanza Acuña.
             </p>
           </div>
+
+          <div style="position:relative;padding-top:max(60%,324px);width:100%;height:0;"><iframe style="position:absolute;border:none;width:100%;height:100%;left:0;top:0;" src="https://online.fliphtml5.com/ArenaDigital/jfgs/"  seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" ></iframe></div>
+          
         </section>
+        
 
 {/* RESEÑA + ACERCA DE LA AUTORA (2 columnas en desktop, 1 en móvil) */}
 <section id="resena-autora" className="section">
