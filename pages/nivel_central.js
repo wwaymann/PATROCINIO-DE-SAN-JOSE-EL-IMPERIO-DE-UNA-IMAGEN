7c8d16bad4aaa2a78f8dba76729aa1addf9fafe1
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-11-20 01:40:57 -0300

    Create nivel_central.js

---
+++ pages/nivel_central.js
@@ -0,0 +1,20 @@
+import MultimediaLevel from "../components/MultimediaLevel";
+
+const PERSONAJES_NIVEL_CENTRAL = [
+  {
+    id: "trinidad",
+    nombre: "santísima trinidad",
+    descripcion: "representación de dios padre, hijo y espíritu santo.",
+    src: "/capas/Trinidad_full.png",
+  },
+];
+
+export default function Nivel_central() {
+  return (
+    <MultimediaLevel
+      titulo="Tercer Nivel"
+      background="/fondos/TercerNivel.png"
+      personajes={PERSONAJES_NIVEL3}
+    />
+  );
+}
