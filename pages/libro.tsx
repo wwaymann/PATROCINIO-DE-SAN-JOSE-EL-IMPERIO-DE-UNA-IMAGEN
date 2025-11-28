66f68bb5aeca7f87b7f73118d32dd711dcb93d53
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-11-28 13:25:07 -0300

    Create libro.tsx

---
+++ pages/libro.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import H5P from './h5p'; // Ajusta la ruta si es necesario
+
+const Libro = () => {
+  return (
+    <div>
+      <h1>Mi Libro Interactivo</h1>
+      <H5P /> {/* Aquí insertas el componente H5P */}
+    </div>
+  );
+};
+
+export default Libro;
