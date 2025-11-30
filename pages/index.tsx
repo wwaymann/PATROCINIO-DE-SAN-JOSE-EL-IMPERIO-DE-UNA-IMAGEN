40b29e7386b7f5bdb1775ec6e1a1ba0afb4d0b56
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-11-29 22:24:00 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -440,24 +440,8 @@ END:VCALENDAR`
               height: 0
             }}
           >
-            <iframe
-              style={{
-                position: 'absolute',
-                border: 'none',
-                width: '100%',
-                height: '100%',
-                left: 0,
-                top: 0
-              }}
-              src="https://online.fliphtml5.com/ArenaDigital/jfgs/"
-              title="Vista previa del eBook — FlipHTML5"
-              loading="lazy"
-              scrolling="no"
-              frameBorder={0}
-              allowFullScreen
-            />
-
             <iframe allowfullscreen="allowfullscreen" allow="clipboard-write" scrolling="no" class="fp-iframe" src="https://heyzine.com/flip-book/bae6eed35a.html" style="border: 1px solid lightgray; width: 100%; height: 400px;"></iframe>
+
           </div>
         </section>
 
