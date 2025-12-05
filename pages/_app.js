7354be3f0c9d25c160a0e93b9b52aa16591ee958
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-12-04 22:49:07 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+        src="https://www.googletagmanager.com/gtag/js?id=G-GCD777XB29"
         strategy="afterInteractive"
       />
       <Script id="ga4-init" strategy="afterInteractive">
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-XXXXXXX');
+          gtag('config', 'G-GCD777XB29');
         `}
       </Script>
       <Component {...pageProps} />
